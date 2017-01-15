693852407748d432aab658777e872fe116d23d4a
coffsj <coffsj@coffsj-PC>
2017-01-16 00:03:30 +0700

    Add FileNotFoundException

--- FileMonitor/src/ConfigSetting.java
+++ FileMonitor/src/ConfigSetting.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -8,8 +9,9 @@ public class ConfigSetting {
 	public static String dirPath;
 	public static String dst_fileName;
 	public static String monitorCycle;
-	private String cfgFileName="D:/workspace/GitRepo/FileMonitor/conf";
-	// private String cfgDirName="D:/workspace/java/FileMonitoring/conf/config";
+	// private String cfgFileName="D:/workspace/conf/config.properties";
+	// private String cfgFileName="D:/workspace/java/conf/config.properties";
+	private String cfgFileName="D:/workspace/GitRepo/FileMonitor/conf/config.properties";
 	
 	// Init 
 	public void initDir() {
@@ -22,6 +24,10 @@ public class ConfigSetting {
 		try (InputStream inputStream = new FileInputStream(filePath)) {
 			// Loading the properties. 
 			prop.load(inputStream);
+		} catch (FileNotFoundException e) {
+			System.out.println("Cannot find the file. Check the location.");
+			e.printStackTrace();
+			return;
 		} catch (IOException ex) {
 			System.out.println("Problem occurs when reading file.");
 			ex.printStackTrace();
